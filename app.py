367ab67860eb7adc833c07dd9de470ea4b99ed3d
Ayush Jakhmola <ayusjakhmola25@gmail.com>
2026-03-11 17:27:13 +0530

    `Update MAIL_USERNAME configuration to 'healthycafe2025@gmail.com'`

--- app.py
+++ app.py
@@ -112,7 +112,7 @@ try:
     app.config['MAIL_SERVER'] = 'smtp.gmail.com'
     app.config['MAIL_PORT'] = 465
     app.config['MAIL_USE_SSL'] = True
-    app.config['MAIL_USERNAME'] = 'Healthy Cafe'
+    app.config['MAIL_USERNAME'] = 'healthycafe2025@gmail.com'
     app.config['MAIL_PASSWORD'] = 'yrzljphqtcrywzmd'
     app.config['MAIL_DEFAULT_SENDER'] = 'healthycafe2025@gmail.com'
     
